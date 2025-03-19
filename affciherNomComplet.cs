7df3d5ab205a6551845bc44c9ddf492eb5e4df60
¨Njountou1¨ <njountousamir01@gmail.com>
2025-03-18 21:31:28 -0400

    Commit initial

--- affciherNomComplet.cs
+++ affciherNomComplet.cs
@@ -5,7 +5,6 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, Git! Je m'appelle <Njountou Ndiandji Abdoul Samir>");
-       Console.WriteLine("En majuscule" + "Hello, Git! Je m'appelle <Votre Nom>".ToUpper());
     }
 
 }
