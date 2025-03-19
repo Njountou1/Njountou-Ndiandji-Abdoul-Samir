c73703e01f388928fbe9e5864dcfa3fcd8c177a6
¨Njountou1¨ <njountousamir01@gmail.com>
2025-03-18 21:32:16 -0400

    Ajout de l'affichage en majuscule

--- affciherNomComplet.cs
+++ affciherNomComplet.cs
@@ -5,6 +5,7 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, Git! Je m'appelle <Njountou Ndiandji Abdoul Samir>");
+       Console.WriteLine("En majuscule" + "Hello, Git! Je m'appelle <Votre Nom>".ToUpper());
     }
 
 }
